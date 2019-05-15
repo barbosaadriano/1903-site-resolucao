71dcf630dd4935a98997ede7c97035d706a03fb3
Adriano Barbosa <b.adrianobarbosa@gmail.com>
2019-05-14 22:22:43 -0300

    ajustes realizados

--- src/Services/ManualPostProvider.php
+++ src/Services/ManualPostProvider.php
@@ -9,11 +9,11 @@ class ManualPostProvider implements PostProviderInterface
 
     public function getUltimos() {
         $out = [];
-        for($i=0; $i<10; $i++) {
+        for($i=0; $i<6; $i++) {
             $out[] = [ 
                 'titulo' => "Post ".($i+1),
                 'conteudo' => "Post para teste ".($i+1),
-                'miniatura' => 'https://picsum.photos/200',
+                'miniatura' => 'https://picsum.photos/300?rand='.rand(0,1000),
                 'autor'=> 'fulano de tal'
             ];
         }
